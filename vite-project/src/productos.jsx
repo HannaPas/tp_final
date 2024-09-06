e2d1a213479120f6c3201a3c236a2829e895f3d6
Hanna Pastor <101837322+HannaPas@users.noreply.github.com>
2024-09-06 01:09:19 -0300

    dir

---
+++ vite-project/src/productos.jsx
@@ -0,0 +1,6 @@
+function Productos() {
+    return <h2>Página de Productos</h2>;
+  }
+  
+  export default Productos;
+  
---
+++ vite-project/src/registro.jsx
@@ -0,0 +1,6 @@
+function Registro() {
+    return <h2>Página de Registro</h2>;
+  }
+  
+  export default Registro;
+  
